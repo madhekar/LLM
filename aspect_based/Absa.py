a0817c56a9e5813b0c7554cf5e9a6d6e224a949b
Bhalchandra Madhekar <bmadhekar@gmail.com>
2023-12-12 11:32:49 -0800

    aspect based sentiment class

--- aspect_based/Absa.py
+++ aspect_based/Absa.py
@@ -1,37 +1,41 @@
 
-
+# import pre-trained tokenizer and model
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch.nn.functional as F
 from transformers import pipeline
 
-# lead aspects based pre-trained model 
-absa_tokenizer = AutoTokenizer.from_pretrained("yangheng/deberta-v3-base-absa-v1.1")
-absa_model = AutoModelForSequenceClassification.from_pretrained("yangheng/deberta-v3-base-absa-v1.1")
 
-text = "Our birthday party diner had great experience at the solidad restaurant, food was delicious as usual," \
-"but the service was ok, photo service was excellent."
+class ABSA():
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained("yangheng/deberta-v3-base-absa-v1.1")
+        self.absa_model = AutoModelForSequenceClassification.from_pretrained("yangheng/deberta-v3-base-absa-v1.1")
+        self.classifier = pipeline("text-classification", model=self.absa_model, tokenizer=self.tokenizer)
 
-# ABSA of food
-aspect = "food"
-input = absa_tokenizer(f"[CLS] {text} [SEP] {aspect} [SEP]", return_tensors="pt")
-outputs = absa_model(**input)
-probs = F.softmax(outputs.logits, dim=1)
-probs = probs.detach().numpy()[0]
+    def getAspectScoring(self, aspect, text):
+        input = self.tokenizer(f"[CLS] {text} [SEP] {aspect} [SEP]", return_tensors="pt")
+        outputs = self.absa_model(**input)
+        probs = F.softmax(outputs.logits, dim=1)
+        probs = probs.detach().numpy()[0]       
+     
+        print(f"sentense : {text}")
+        print("*****")
 
-print(f"sentense : {text}")
-print("*****")
-print(f"Sentiment of {aspect}: ")
-for prob, label in zip(probs, ["neg", "neu", "pos"]):
-    print (f"Label {label}: {prob}")
-print("*****")
+        print(f"Sentiment of {aspect}: ")
+        for prob, label in zip(probs, ["neg", "neu", "pos"]):
+            print (f"Label {label}: {prob}")
+        print("*****")
 
-aspect = "service"
-input = absa_tokenizer(f"[CLS] {text} [SEP] {aspect} [SEP]", return_tensors="pt")
-outputs = absa_model(**input)
-probs = F.softmax(outputs.logits, dim=1)
-probs = probs.detach().numpy()[0]
+    def getMAspects(self, aspects, text):
+        for aspect in aspects:
+            print(aspect, self.classifier(text, text_pair=aspect))
 
-print(f"Sentiment of {aspect}: ")
-for prob, label in zip(probs, ["neg", "neu", "pos"]):
-    print (f"Label {label}: {prob}")    
-print("*****")
+if __name__=="__main__":
+    absa = ABSA()
+    text = "Our birthday party diner had great experience at the Zyper restaurant, food was delicious as usual," \
+"but the service was average, photo service was exquizit."    
+    aspect = "food"
+    absa.getAspectScoring(aspect=aspect, text=text) 
+
+    # multiple aspects in list
+    absa.getMAspects(aspects=["food", "service"], text=text)
+
